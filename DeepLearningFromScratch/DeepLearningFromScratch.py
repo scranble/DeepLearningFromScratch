9f0dd9fc2a36211b8657e660fd6fdab2a340b467
scranble <scranble@users.noreply.github.com>
2017-10-14 13:12:21 +0900

    add Multiplier Layer and Adder Layer, section 5.4

--- DeepLearningFromScratch/DeepLearningFromScratch.py
+++ DeepLearningFromScratch/DeepLearningFromScratch.py
@@ -110,3 +110,103 @@ A3 = np.dot(Z2,W3)+B3
 print(A3)
 Y = identity_function(A3)
 print(Y)
+
+# multiplier layer
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+        return out
+    
+    # dout / dx = (dout / dz) * (dz / dx)
+    def backword(self, dout):
+        dx = dout * self.y # dz / dx = d/dx (xy)
+        dy = dout * self.x
+        return dx, dy
+
+# layer example
+apple = 100
+apple_num = 2
+tax_rate = 1.1
+
+mul_apple_layer = MulLayer() # new object instance
+mul_tax_layer = MulLayer()
+
+# forward propergation
+def MulLayer_Example(apple, apple_num, mul_apple_layer, mul_tax_layer):
+    apple_price = mul_apple_layer.forward(apple, apple_num)
+    price = mul_tax_layer.forward(apple_price, tax_rate) # should be casted
+
+    print(price) # 220
+    return price
+
+price = MulLayer_Example(apple, apple_num, mul_apple_layer, mul_tax_layer)
+
+# backward propergation
+def MulLayer_Backword_Example(mul_apple_layer, mul_tax_layer):
+    dprice = 1
+    dapple_price, dtax = mul_tax_layer.backword(dprice)
+    dapple, dapple_num = mul_apple_layer.backword(dapple_price)
+    print(dapple, dapple_num, dtax) # 2,2 110 200
+    return dapple, dapple_num, dtax
+
+dapple, dapple_num, dtax  = MulLayer_Backword_Example(mul_apple_layer, mul_tax_layer)
+
+# adder layer
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+    # dout / dx = (dout / dz) * (dz / dx)
+    # dz / dx = d/dx (x + y)
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+# example
+# ( 100 * 2 + 150 * 3 ) * 1.1
+orange = 150
+orange_num = 3
+
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+
+def Mul_And_Add_Forward_Example(apple, apple_num, orange, orange_num, tax_rate,
+                                add_apple_orange_layer, 
+                                mul_apple_layer,
+                                mul_orange_layer,
+                                mul_tax_layer):
+    apple_price = mul_apple_layer.forward(apple, apple_num)
+    orange_price = mul_orange_layer.forward(orange, orange_num)
+    all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+    price = mul_tax_layer.forward(all_price, tax_rate)
+    
+    print(price)
+    return price
+
+price = Mul_And_Add_Forward_Example(apple, apple_num, orange, orange_num, tax_rate,
+                                    add_apple_orange_layer,
+                                    mul_apple_layer,
+                                    mul_orange_layer,
+                                    mul_tax_layer)
+ # 715
+
+dprice = 1
+def Mul_And_Add_BackWard_Example(dprice):
+    dall_price, dtax = mul_tax_layer.backword(dprice)
+    dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)
+    dorange, dorange_num = mul_orange_layer.backword(dorange_price)
+    dapple, dapple_num = mul_apple_layer.backword(dapple_price)
+    print(dapple, dapple_num, dorange, dorange_num, dtax) # 2.2, 110, 3.3, 165, 650
+    return dapple, dapple_num, dorange, dorange_num, dtax
+
